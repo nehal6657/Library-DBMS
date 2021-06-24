58e1f72cde0a266ee18307f8bdb7afae8978e150
nehal6657 <nehalreshuaaryan@gmail.com>
2021-06-24 06:11:33 +0530

    added frontend for admin dashboard

--- admin_home.php
+++ admin_home.php
@@ -305,6 +305,74 @@ while ($row = mysqli_fetch_assoc($query_run)) {
 
           <!--adding authors end-->
           <div class="main_site">
+            <div class="dashhh">
+            <div class="container maxw">
+            <div class="row">
+              <div class="col m-1.2">
+                <div class="card s1">
+                    <div class="card-header title2">
+                      Number of registered Users
+                    </div>
+                  <div class="card-body">
+                    <h3 class="card-text"> Number of students: </h3>
+                    <p class="card-text">BTech Students: </p>
+                    <p class="card-text">MTech Students: </p>
+                    <p class="card-text">PhD Students: </p>
+
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col m-1.2">
+                <div class="card s1" >
+                    <div class="card-header title2">
+                      Number of Books issued 
+                    </div>
+                  <div class="card-body">
+                    
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+                </div>
+              </div>
+              <div class="w-100"></div>
+              <div class="col m-1.2">
+              <div class="card s1" >
+                    <div class="card-header title2">
+                      Number of Books overdue:
+                    </div>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col m-1.2">
+              <div class="card s1" >
+                  <div class="card-header title2">
+                      Number of Books issued 
+                    </div>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            <div class="btq1 d-flex justify-content-center"><button type="button" class="btn btn-primary s2">Issue Books</button></div>
+          </div>
+
+          </div>
             <div class="View_deatails" id="View_deatails">
               <div class="form">
                 <img src="./style/logo.png" alt="" height="90px" width="auto" />
